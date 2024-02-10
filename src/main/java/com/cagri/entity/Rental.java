5fa562f88f5e603e1e0ff367e34a7d08d2ebe5ad
PxOctopus <cagrigumuskaptan@gmail.com>
2024-02-10 22:13:42 +0300

    2nd attempt!

--- src/main/java/com/cagri/entity/Rental.java
+++ src/main/java/com/cagri/entity/Rental.java
@@ -2,20 +2,21 @@ package com.cagri.entity;
 
 import com.cagri.utility.DataBase;
 
+import java.awt.image.DataBuffer;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Rental extends DataBase implements IRental{
+public class Rental implements IRental{
     private LocalDate issueDate;
     private LocalDate returnDate;
     private final double feePerDay = 0.75;
     private Book book;
     private Customer customer;
     private Cashier cashier;
-    ArrayList<Book> bookList = new ArrayList<>();
-    ArrayList<Book> rentedBooks = new ArrayList<>();
+//    ArrayList<Book> bookList = new ArrayList<>();
+
 
     // P. CONSTRUCTORS //
     public Rental(LocalDate issueDate, LocalDate returnDate, Book book, Customer customer, Cashier cashier) {
@@ -38,18 +39,17 @@ public class Rental extends DataBase implements IRental{
     public void searchNrentBook() {
         System.out.print("Please enter a book name that you want to search: ");
         String book = new Scanner(System.in).nextLine();
-        for (int i = 0; i < bookList.size(); i++) {
-            if (bookList.get(i).getName().equalsIgnoreCase(book) && bookList.get(i).isIssued()==false) {
+        for (int i = 0; i < DataBase.bookList.size(); i++) {
+            if (DataBase.bookList.get(i).getName().equalsIgnoreCase(book) && DataBase.bookList.get(i).isIssued()==false) {
                 System.out.println(book + " is available!" + " \nWould you like to rent the book? Y/N");
                 String answer = new Scanner(System.in).nextLine();
                 if (answer.equalsIgnoreCase("Y")) {
-                    bookList.get(i).setIssued(true);
-                    rentedBooks.add(bookList.get(i));
+                    DataBase.bookList.get(i).setIssued(true);
                     System.out.println(book + " was successfully rented!");
                 } else if (answer.equalsIgnoreCase("N")) {
                     System.out.println("Exited");
                 }
-            } else if (bookList.get(i).isIssued()) {
+            } else if (DataBase.bookList.get(i).isIssued()) {
                 System.out.println("Book's already rented!");
             } else {
                 System.out.println("Not found!");
@@ -59,7 +59,7 @@ public class Rental extends DataBase implements IRental{
 
     // PRINT RENTED BOOK LIST //
     public void getRentedBookList() {
-        System.out.println(rentedBooks);
+        System.out.println(bookList.);
     }
 
 
@@ -67,12 +67,11 @@ public class Rental extends DataBase implements IRental{
     public void searchCustomer() {
         System.out.print("Please enter a customer name that you want to search: ");
         String customer = new Scanner(System.in).nextLine();
-        for (int i = 0; i < customerList.size(); i++) {
-            if (customerList.get(i).getUsername().equalsIgnoreCase(customer)) {
+        for (int i = 0; i < DataBase.customerList.size(); i++) {
+            if (DataBase.customerList.get(i).getUsername().equalsIgnoreCase(customer)) {
                 System.out.println(customer + " is matched our records!");
             } else {
                 System.out.println("Customer not found!" + "\nPlease check the rented books on the list!");
-
             }
         }
     }
@@ -82,8 +81,8 @@ public class Rental extends DataBase implements IRental{
     public void findCustomerByBookId() {
         System.out.print("Please enter a book ID you want to find the renter: ");
         int bookID = new Scanner(System.in).nextInt();
-        for (int i = 0; i < bookList.size(); i++) {
-            if (bookList.get(i).getBookId() == bookID)
+        for (int i = 0; i < DataBase.bookList.size(); i++) {
+            if (DataBase.bookList.get(i).getBookId() == bookID)
                 System.out.println("Rented by: " + customer.getUsername());
         }
         System.out.println("No matching results were found!");
