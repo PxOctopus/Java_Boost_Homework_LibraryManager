901df7bd2cab8548fa994207afc2a752604f4bc4
PxOctopus <cagrigumuskaptan@gmail.com>
2024-02-10 23:08:19 +0300

    2nd attempt!

--- src/main/java/com/cagri/service/BookService.java
+++ src/main/java/com/cagri/service/BookService.java
@@ -1,4 +1,28 @@
 package com.cagri.service;
 
+import com.cagri.entity.Book;
+import com.cagri.repository.BookRepository;
+import com.cagri.repository.IBookRepository;
+import com.cagri.utility.DataBase;
+
+import java.util.ArrayList;
+
 public class BookService implements IBookService{
+    private IBookRepository bookRepository;
+
+    public BookService(){
+        bookRepository = new BookRepository();
+    }
+    public void save(Book book) {
+       bookRepository.save(book);
+    }
+
+    public void delete(int index) {
+       bookRepository.delete(index);
+
+    }
+
+    public ArrayList<Book> findAll() {
+        return bookRepository.findAll();
+    }
 }
--- src/main/java/com/cagri/service/CashierService.java
+++ src/main/java/com/cagri/service/CashierService.java
@@ -1,4 +1,28 @@
 package com.cagri.service;
 
+import com.cagri.entity.Book;
+import com.cagri.repository.BookRepository;
+import com.cagri.repository.IBookRepository;
+import com.cagri.repository.ICashierRepository;
+
+import java.util.ArrayList;
+
 public class CashierService implements ICashierService{
+    private ICashierRepository cashierRepository;
+
+    public BookService(){
+        bookRepository = new BookRepository();
+    }
+    public void save(Book book) {
+        bookRepository.save(book);
+    }
+
+    public void delete(int index) {
+        bookRepository.delete(index);
+
+    }
+
+    public ArrayList<Book> findAll() {
+        return bookRepository.findAll();
+    }
 }
