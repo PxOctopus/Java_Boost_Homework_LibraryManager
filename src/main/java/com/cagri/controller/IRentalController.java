5dc35b36688ad6f510ef68ca9786d39ff0075bc7
PxOctopus <cagrigumuskaptan@gmail.com>
2024-02-11 13:35:21 +0300

    Going Back!

--- src/main/java/com/cagri/controller/IRentalController.java
+++ src/main/java/com/cagri/controller/IRentalController.java
@@ -1,9 +1,5 @@
 package com.cagri.controller;
 
 public interface IRentalController {
-    public void searchNrentBook();
-    public void getRentedBookList();
-    public void searchCustomer();
-    public void findCustomerByBookId();
-    public void calculateFee();
+
 }
--- src/main/java/com/cagri/entity/Rental.java
+++ src/main/java/com/cagri/entity/Rental.java
@@ -17,7 +17,6 @@ public class Rental{
     private Cashier cashier;
 
 
-
     public Rental(LocalDate issueDate, LocalDate returnDate, Book book, Customer customer, Cashier cashier) {
         this.issueDate = issueDate;
         this.returnDate = returnDate;
@@ -91,4 +90,79 @@ public class Rental{
         sb.append('}');
         return sb.toString();
     }
+
+    /************************REQUESTED METHODS************************************/
+
+    // SEARCH & RENT A BOOK //
+    public void searchNrentBook() {
+        System.out.print("Please enter a book name that you want to search: ");
+        String book = new Scanner(System.in).nextLine();
+        for (int i = 0; i < DataBase.bookList.size(); i++) {
+            if (DataBase.bookList.get(i).getName().equalsIgnoreCase(book) && !DataBase.bookList.get(i).isIssued()) {
+                System.out.println(book + " is available!" + " \nWould you like to rent the book? Y/N");
+                String answer = new Scanner(System.in).nextLine();
+                if (answer.equalsIgnoreCase("Y")) {
+                    DataBase.bookList.get(i).setIssued(true);
+                    System.out.println(book + " was successfully rented!");
+                } else if (answer.equalsIgnoreCase("N")) {
+                    System.out.println("Exited");
+                }
+            } else if (DataBase.bookList.get(i).isIssued()) {
+                System.out.println("Book's already rented!");
+            } else {
+                System.out.println("Not found!");
+            }
+        }
+    }
+
+    // PRINT RENTED BOOK LIST //
+    public void getRentedBookList() {
+        for (int i=0; i<DataBase.bookList.size();i++){
+            if (DataBase.bookList.get(i).isIssued){
+                System.out.println("Rented books are: " + DataBase.bookList.get(i).getName());
+            }else{
+                System.err.println("No rented books found!");
+            }
+        }
+    }
+
+
+    // SEARCH A CUSTOMER //
+    public void searchCustomer() {
+        System.out.print("Please enter a customer name that you want to search: ");
+        String customer = new Scanner(System.in).nextLine();
+        for (int i = 0; i < DataBase.customerList.size(); i++) {
+            if (DataBase.customerList.get(i).getUsername().equalsIgnoreCase(customer)) {
+                System.out.println(customer + " is matched our records!");
+            } else {
+                System.out.println("Customer not found!" + "\nPlease check the rented books on the list!");
+            }
+        }
+    }
+
+
+    // FIND CUSTOMER BY BOOKID //
+    public void findCustomerByBookId() {
+        System.out.print("Please enter a book ID you want to find the renter: ");
+        int bookID = new Scanner(System.in).nextInt();
+        for (int i = 0; i < DataBase.bookList.size(); i++) {
+            if (DataBase.bookList.get(i).getBookId() == bookID)
+                System.out.println("Rented by: " + customer.getUsername());
+        }
+        System.out.println("No matching results were found!");
+    }
+
+
+    // CALCULATING FEE //
+    public void calculateFee() {
+        System.out.println("Calculating Fee...");
+        System.out.println("Fee: " + (ChronoUnit.DAYS.between(issueDate, returnDate) * feePerDay));
+    }
+
+    // FIND PREVIOUS BORROWERS //
+
+    // FIND CUSTOMERS' NAMES STARTS WITH "AHM" //
+
+    // FIND CUSTOMERS' BORROWED BOOKLIST //
+
 }
