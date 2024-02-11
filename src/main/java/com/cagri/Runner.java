b39886dc7ea7452645b5a430ebae4f1b6312dd35
PxOctopus <cagrigumuskaptan@gmail.com>
2024-02-11 18:03:20 +0300

    5

--- src/main/java/com/cagri/Runner.java
+++ src/main/java/com/cagri/Runner.java
@@ -13,16 +13,27 @@ import static com.cagri.utility.DataBase.*;
 public class Runner {
     public static void main(String[] args) {
 
-        Book book1 = new Book(1, "Das Kapital", "Karl Marx", "Treatise", false);
-        Customer customer1 = new Customer(1, "Cagri Gumuskaptan", "Samsun", "5354942288");
-        Cashier cashier1 = new Cashier(1, "Hasan Inceler", "Istanbul", "05550001122");
-        bookList.add(book1);
+        Book book1 = new Book(1, "Martin Eden", "Jack London", "Novel", false);
+        Book book2 = new Book(2, "After The Ball", "Leo Tolstoy", "Short Story", false);
+        Book book3 = new Book(3, "Guns, Germs, and Steel", "Jared Diamond", "Popular Science", false);
+        Book book4 = new Book(4, "The Plague", "Albert Camus", "Novel", false);
+        Book book5 = new Book(5, "The Triple Helix", "Richard Lewontin", "Science", false);
+        Collections.addAll(bookList, book1, book2, book3, book4, book5);
+
+        Customer customer1 = new Customer(1, "Cagri Gumuskaptan", "Samsun", "5294212348");
+        Customer customer2 = new Customer(2, "Sevgin Gumuskaptan", "Samsun", "5294231122");
+        Customer customer3 = new Customer(3, "Zeynep Oyku Buyukbay", "Ankara", "5193216677");
+        Collections.addAll(customerList, customer1, customer2, customer3);
 
 
-//       ******************************* Collections.addAll(bookList, book1, )*****************************************************
-        customerList.add(customer1);
-//       customer1.getUsername();
-        cashierList.add(cashier1);
+        Cashier cashier1 = new Cashier(1, "Hasan Inceler", "Istanbul", "05110001122");
+        Cashier cashier2 = new Cashier(2, "Erman Caglar", "Istanbul", "05102203322");
+        Collections.addAll(cashierList, cashier1, cashier2);
+
+
+
+
+
         Rental rent1 = new Rental(book1, customer1, cashier1);
         LocalDate issueDate = LocalDate.parse("2024-01-24");
         LocalDate returnDate = LocalDate.parse("2024-01-30");
